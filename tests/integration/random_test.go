511f015cb3f67dfa8b66d9b4d23e350d3bfff188
royalt-lb <royalt@lightbitslabs.com>
2024-10-28 11:39:06 +0000

    tests/integration/random_test.go: Random Selector Tests
    This adds an integration test for the Random Selector.
    Verifying that it does not select sequentially.
    This does not validate actual randomness, as this would be difficult to
    determine. So technically just returning the reversed list would already
    provide the correct result here to fulfill the test requirements, but
    that is beyond the scope for this test.
    
    Signed-off-by: royalt-lb <royalt@lightbitslabs.com>

---
+++ tests/integration/random_test.go
@@ -0,0 +1,66 @@
+//go:build integration
+// +build integration
+
+package integration
+
+import (
+	api "balance/gen"
+	"context"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func TestRandomSanity(t *testing.T) {
+	ctx, cli, slbContainerID, backendIDs := setupSlbWithBackends(t, 4)
+	t.Cleanup(func() {
+		time.Sleep(1)
+		o, _ := getContainerLogs(ctx, cli, slbContainerID)
+		t.Log(o)
+		cleanupContainer(context.Background(), cli, slbContainerID)
+		for _, backend := range backendIDs {
+			cleanupContainer(context.Background(), cli, backend)
+		}
+	})
+	slbIp, err := getContainerIP(slbContainerID)
+	require.NoError(t, err)
+	// get backend ids to ips
+	config := &api.Config{
+		Strategy:   api.SelectorStrategy_SELECTOR_STRATEGY_RANDOM,
+		Endpoints:  make([]*api.Server, 0),
+		ListenPort: backendListenPort,
+		// ListenAddress: slbIp,
+	}
+	for _, id := range backendIDs {
+		ip, err := getContainerIP(id)
+		require.NoError(t, err)
+		require.NotEmpty(t, ip)
+		s := &api.Server{Address: ip}
+		config.Endpoints = append(config.Endpoints, s)
+	}
+	apiClient, err := newApiClient(slbIp, "443")
+	require.NoError(t, err)
+	_, err = apiClient.Configure(ctx, config)
+	require.NoError(t, err)
+	_, err = apiClient.Run(ctx, &emptypb.Empty{})
+	require.NoError(t, err)
+
+	var selectedBackendIds []string
+
+	for _ = range backendIDs {
+		// send request via slb and retrieve the container id of the backend.
+		slbUrl := "http://" + slbIp + ":" + config.ListenPort
+		res, err := http.Get(slbUrl)
+		require.NoError(t, err)
+		body, err := io.ReadAll(res.Body)
+		require.NoError(t, err)
+		nlbRespID := string(body[:11])
+		require.Contains(t, backendIDs, nlbRespID)
+		selectedBackendIds = append(selectedBackendIds, nlbRespID)
+	}
+	require.NotEqualValues(t, backendIDs, selectedBackendIds, "Backends selected sequentially")
+}
