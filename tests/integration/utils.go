9eb77590725ce4986687b4ff7fc6dcba00f2b9f8
royalt-lb <royalt@lightbitslabs.com>
2024-10-28 11:37:37 +0000

    tests/integration/docker_harness.go: Cosmetic
    Moved commonly used constants and functions to allow
     more tests to utilize these functions where they are
    logically expected to be, and makes it easier to develop further tests.
    
    Signed-off-by: royalt-lb <royalt@lightbitslabs.com>

--- tests/integration/utils.go
+++ tests/integration/utils.go
@@ -5,7 +5,15 @@ package integration
 
 import (
 	api "balance/gen"
+	"context"
+	"strings"
+	"testing"
 
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -13,4 +21,51 @@ import (
 func newApiClient(serverAddress string, port string) (api.BalanceClient, error) {
 	conn, err := grpc.NewClient(serverAddress+":"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	return api.NewBalanceClient(conn), err
+}
+
+func setup(t *testing.T, name string) (context.Context, *client.Client, string) {
+	ctx := context.Background()
+
+	cli, err := createDockerClient()
+	require.NoError(t, err)
+
+	imageTags := []string{slbImageRepo + imgVersion}
+	config := &container.Config{
+		Image: imageTags[0],
+		ExposedPorts: nat.PortSet{
+			nat.Port(HostPort): struct{}{},
+			backendListenPort:  struct{}{},
+		},
+	}
+	containerID, err := createAndStartContainer(ctx, cli, config, strings.ToLower(t.Name())+"-"+strings.ToLower(name))
+	require.NoError(t, err)
+
+	return ctx, cli, containerID
+}
+
+func setupSlbWithBackends(t *testing.T, numBackends int) (context.Context, *client.Client, string, []string) {
+	// setup slb
+	ctx, cli, containerID := setup(t, "slb"+"-"+uuid.NewString()[:4])
+	// setup backends
+	backendContainers := make([]string, 0)
+	for i := 0; i < numBackends; i++ {
+		ctx := context.Background()
+
+		cli, err := createDockerClient()
+		require.NoError(t, err)
+
+		imageTags := []string{BackEndImgName + BackendImgVersion}
+		config := &container.Config{
+			Image: imageTags[0],
+			ExposedPorts: nat.PortSet{
+				"443":             struct{}{},
+				backendListenPort: struct{}{},
+			},
+		}
+		backendContainerID, err := createAndStartContainer(ctx, cli, config, strings.ToLower(t.Name())+"-"+"backend-"+uuid.NewString()[:4])
+		require.NoError(t, err)
+		// required is the shortened id here as the backend provides the full id.
+		backendContainers = append(backendContainers, backendContainerID[:11])
+	}
+	return ctx, cli, containerID, backendContainers
 }
