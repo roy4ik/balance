15800152cc42f4587ae3459b6e50c290cc582ac3
roy4ik <royalt@lightbitslabs.com>
2025-01-21 12:39:48 +0000

    tests/integration:implement docker_harness_deployment
    This implements the new DockerHarness deployment changes.
    
    Signed-off-by: roy4ik <royalt@lightbitslabs.com>

--- tests/integration/random_test.go
+++ tests/integration/random_test.go
@@ -6,17 +6,21 @@ package integration
 import (
 	api "balance/gen"
 	"balance/internal/apiService"
+	"context"
 	"io"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func TestRandomSanity(t *testing.T) {
-	ctx, _, slbContainerID, backendIDs, certDir := setupSlbWithBackends(t, 4)
-	slbIp, err := getContainerIP(slbContainerID)
+	deployment := NewDockerDeployment(t)
+	slbContainerID, certDir := deployment.setup()
+	backendIDs := deployment.setupBackends(4, certDir)
+	slbIp, err := deployment.getIP(slbContainerID)
 	require.NoError(t, err)
 	// get backend ids to ips
 	config := &api.Config{
@@ -26,7 +30,7 @@ func TestRandomSanity(t *testing.T) {
 		// ListenAddress: slbIp,
 	}
 	for _, id := range backendIDs {
-		ip, err := getContainerIP(id)
+		ip, err := deployment.getIP(id)
 		require.NoError(t, err)
 		require.NotEmpty(t, ip)
 		s := &api.Server{Address: ip}
@@ -34,23 +38,29 @@ func TestRandomSanity(t *testing.T) {
 	}
 	apiClient, err := newApiClient(certDir, slbIp, apiService.DefaultApiPort)
 	require.NoError(t, err)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*30)
+	t.Cleanup(cancel)
 	_, err = apiClient.Configure(ctx, config)
 	require.NoError(t, err)
 	_, err = apiClient.Run(ctx, &emptypb.Empty{})
 	require.NoError(t, err)
 
 	var selectedBackendIds []string
-
-	for _ = range backendIDs {
+	shortenedBackendIDs := make([]string, 0)
+	for _, id := range backendIDs {
+		shortenedBackendIDs = append(shortenedBackendIDs, id[:12])
+	}
+	for range backendIDs {
 		// send request via slb and retrieve the container id of the backend.
 		slbUrl := "http://" + slbIp + ":" + config.ListenPort
 		res, err := http.Get(slbUrl)
 		require.NoError(t, err)
 		body, err := io.ReadAll(res.Body)
 		require.NoError(t, err)
-		nlbRespID := string(body[:11])
-		require.Contains(t, backendIDs, nlbRespID)
+		nlbRespID := string(body)
+		require.Contains(t, shortenedBackendIDs, nlbRespID)
 		selectedBackendIds = append(selectedBackendIds, nlbRespID)
 	}
+
 	require.NotEqualValues(t, backendIDs, selectedBackendIds, "Backends selected sequentially")
 }
